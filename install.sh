9b7dd08fd5f1d3f353a373175e0f938c2a9dcc8e
ws.jung.d <ws.jung.d@gmail.com>
2018-10-05 17:06:33 +0900

    polybar gmail notifier add

--- install.sh
+++ install.sh
@@ -1,44 +1,70 @@
-sudo reflector --latest 20 --age 24 --sort rate --save /etc/pacman.d/mirrorlist
-sudo pacman -Syyu --noconfirm && sudo pacman -Scc --noconfirm
-sudo pacman -S uim --noconfirm
-sudo pacman -S noto-fonts-cjk --noconfirm
-sudo pacman -S ack --noconfirm
-sudo pacman -S tree --noconfirm
-sudo pacman -S vim --noconfirm
-sudo pacman -Rns firefox --noconfirm
-sudo pacman -Rns chromium --noconfirm
-yaourt -S google-chrome --noconfirm
-yaourt -S audacious --noconfirm
-yaourt -S polybar --noconfirm
-yaourt -S albert --noconfirm
+BASEDIR=$(readlink -f "$0"| sed -e 's/\/connect.sh//g')
 
-curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install java 8.0.181-zulu
-sdk install gradle 3.2.1
+FUNC=$1
 
-#yaourt -S intellij-idea-ultimate-edition --noconfirm
-#yaourt -S datagrip --noconfirm
+function installPackages() {
+    sudo reflector --latest 20 --age 24 --sort rate --save /etc/pacman.d/mirrorlist
+    sudo pacman -Syyu --noconfirm && sudo pacman -Scc --noconfirm
+    sudo pacman -S uim --noconfirm
+    sudo pacman -S noto-fonts-cjk --noconfirm
+    sudo pacman -S ack --noconfirm
+    sudo pacman -S tree --noconfirm
+    sudo pacman -S vim --noconfirm
+    sudo pacman -Rns firefox --noconfirm
+    sudo pacman -Rns chromium --noconfirm
+    yaourt -S google-chrome --noconfirm
+    yaourt -S audacious --noconfirm
+    yaourt -S polybar --noconfirm
+    yaourt -S albert --noconfirm
 
-git config --global user.email "ws.jung.d@gmail.com"
-git config --global user.name "ws.jung.d"
+    curl -s "https://get.sdkman.io" | bash
+    source "$HOME/.sdkman/bin/sdkman-init.sh"
+    sdk install java 8.0.181-zulu
+    sdk install gradle 3.2.1
 
-# vim
+    #yaourt -S intellij-idea-ultimate-edition --noconfirm
+    #yaourt -S datagrip --noconfirm
 
+    git config --global user.email "ws.jung.d@gmail.com"
+    git config --global user.name "ws.jung.d"
 
-# fuzzy lock
-yaourt -S betterlockscreen --noconfirm
-yaourt -S xautolock --noconfirm
-
-# dotfiles setting
-sudo pip install dotfiles
-dotfiles --sync --force
+    # vim
 
 
+    # fuzzy lock
+    yaourt -S betterlockscreen --noconfirm
+    yaourt -S xautolock --noconfirm
 
+    # dotfiles setting
+    cp $BASEDIR/dotfilesrc ~/.dotfilesrc
+    sudo pip install dotfiles
+    dotfiles --sync --force
 
-# zsh
-sudo pacman -S zsh --noconfirm
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    # zsh
+    sudo pacman -S zsh --noconfirm
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+}
 
-
+case $FUNC in 
+    "all")
+        installPackages
+        ;;
+    "packages")
+        installPackages
+        ;;
+    "submodule")
+        settingSubmodule
+        ;;
+    "zsh")
+        settingZsh
+        ;;
+    "vim")
+        settingVim
+        ;;
+    "tmux")
+        settingTmux
+        ;;
+   * )
+        exit
+        ;;
+esac
