fccc44927c0b74f9c7b1f935e51e6f6299c2c2a6
Jung Wonseok <ws.jung@anypointmedia.com>
2021-02-14 18:39:58 +0900

    x1

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -12,8 +12,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar > /dev/null; do sleep 0.1; done
 
-MONITOR=DP-0 polybar --reload mainbar-i3 -c ~/.config/polybar/config &
-MONITOR=DP-0 polybar --reload mainbar-i3-bottom -c ~/.config/polybar/config &
+MONITOR=eDP1 polybar --reload mainbar-i3 -c ~/.config/polybar/config &
+MONITOR=eDP1 polybar --reload mainbar-i3-bottom -c ~/.config/polybar/config &
 
-MONITOR=HDMI-0 polybar --reload mainbar-i3-extra -c ~/.config/polybar/config &
-MONITOR=HDMI-0 polybar --reload mainbar-i3-bottom -c ~/.config/polybar/config &
+MONITOR=HDMI1 polybar --reload mainbar-i3 -c ~/.config/polybar/config &
+MONITOR=HDMI1 polybar --reload mainbar-i3-bottom -c ~/.config/polybar/config &
+
+MONITOR=DP2 polybar --reload mainbar-i3 -c ~/.config/polybar/config &
+MONITOR=DP2 polybar --reload mainbar-i3-bottom -c ~/.config/polybar/config &
--- .zshrc
+++ .zshrc
@@ -316,3 +316,6 @@ source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
+
+alias alog='tail -f /var/log/apm/assign-app.log'
+alias elog='tail -f /var/log/apm/epgms-backend.log'
